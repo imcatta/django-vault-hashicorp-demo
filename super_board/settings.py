7d41667fbec57690b54381a87adf02cdb1a1b3f5
Andrea Cattaneo <andrea@cattaneo.dev>
2019-05-09 13:45:59 +0200

    Hard code posgres password in django settings

--- super_board/settings.py
+++ super_board/settings.py
@@ -81,6 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
+        'PASSWORD': 'postgres',
         'HOST': 'db',
         'PORT': 5432,
         'ATOMIC_REQUESTS': True
